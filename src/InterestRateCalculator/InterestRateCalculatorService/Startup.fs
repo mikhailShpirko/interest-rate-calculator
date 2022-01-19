3eb85aaf11cd6003c5a8914916b01ed55f90cf5f
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2022-01-19 19:48:27 +0500

    feat: enabled swagger

--- src/InterestRateCalculator/InterestRateCalculatorService/Startup.fs
+++ src/InterestRateCalculator/InterestRateCalculatorService/Startup.fs
@@ -11,12 +11,18 @@ open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Swashbuckle.AspNetCore.Swagger
+open Microsoft.OpenApi.Models
 
 type Startup(configuration: IConfiguration) =
     member _.Configuration = configuration
 
     // This method gets called by the runtime. Use this method to add services to the container.
     member _.ConfigureServices(services: IServiceCollection) =
+        let info = OpenApiInfo()
+        info.Title <- "Loan Interest Rate Calculator V1"
+        info.Version <- "v1"
+        services.AddSwaggerGen(fun config -> config.SwaggerDoc("v1", info)) |> ignore
         // Add framework services.
         services.AddControllers() |> ignore
 
@@ -25,6 +31,8 @@ type Startup(configuration: IConfiguration) =
         if (env.IsDevelopment()) then
             app.UseDeveloperExceptionPage() |> ignore
         app.UseHttpsRedirection()
+           .UseSwagger()
+           .UseSwaggerUI(fun config -> config.SwaggerEndpoint("/swagger/v1/swagger.json", "Loan Interest Rate Calculator V1"))
            .UseRouting()
            .UseAuthorization()
            .UseEndpoints(fun endpoints ->
