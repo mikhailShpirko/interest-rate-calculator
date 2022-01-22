ecd515985e315669e80e09b1536adfe97e7f953d
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2022-01-22 15:15:17 +0500

    fix: proper type definition for generic constructors

--- src/InterestRateCalculator/InterestRateCalculatorService/Models/Response.fs
+++ src/InterestRateCalculator/InterestRateCalculatorService/Models/Response.fs
@@ -1,13 +1,11 @@
 ﻿namespace InterestRateCalculatorService.Models
 
-open System
-
 type Response<'T> private (value : 'T, isSuccess : bool, errorMessage : string) = 
     member this.IsSuccess = isSuccess
     member this.Value = value
     member this.ErrorMessage = errorMessage
 
-    static member Success<'T>(value : 'T) = Response<'T>(value, true, null)
-    static member Error<'T>(errorMessage : string) = Response<'T>(Unchecked.defaultof<'T>, false, errorMessage)
+    static member Success<'a>(value : 'a) = Response<'a>(value, true, null)
+    static member Error<'a>(errorMessage : string) = Response<'a>(Unchecked.defaultof<'a>, false, errorMessage)
     
 
