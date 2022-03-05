48d0c706a690679628be2fbdf27a63b57227d5b2
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2022-03-05 17:35:16 +0500

    feat: added all services to service discovery, moved services config to consul

--- src/GateWay/WebApi/Startup.cs
+++ src/GateWay/WebApi/Startup.cs
@@ -11,14 +11,11 @@ using WebApi.Configuration.InterestRateCalculatorService;
 using ServiceClients.AmortizationCalculator;
 using ServiceClients.InterestRateCalculator;
 using ServiceClients.Domain;
-using Consul;
-using System;
 
 namespace WebApi
 {
     public class Startup
     {
-        private const string HEALTH_CHECK_ENDPOINT = "HealthCheck";
         public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
@@ -78,10 +75,6 @@ namespace WebApi
                 app.UseDeveloperExceptionPage();        
             }
 
-            if (!env.IsDevelopment())
-            {
-                SetupConsulServiceDiscovery(lifetime);
-            }
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi GateWay v1"));
 
@@ -93,45 +86,7 @@ namespace WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecks($"/{HEALTH_CHECK_ENDPOINT}");
-            });
-        }
-
-        private void SetupConsulServiceDiscovery(IHostApplicationLifetime lifetime)
-        {
-            //consul registration
-            var consulAddress =
-                System.Environment.GetEnvironmentVariable("CONSUL_ADDRESS");
-
-            var serviceAddress =
-                System.Environment.GetEnvironmentVariable("SERVICE_ADDRESS");
-
-            var consulClient = new ConsulClient(x =>
-                x.Address = new Uri(consulAddress));//Consul address requesting registration
-
-            var httpCheck = new AgentServiceCheck()
-            {
-                DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//How long does it take to register after the service starts
-                Interval = TimeSpan.FromSeconds(10),//Health check interval, or heartbeat interval
-                HTTP = $"{serviceAddress}/{HEALTH_CHECK_ENDPOINT}",//Health check address
-                Timeout = TimeSpan.FromSeconds(5)
-            };
-
-            const string serviceName = "GateWay";
-            // Register service with consul
-            var registration = new AgentServiceRegistration()
-            {
-                Checks = new[] { httpCheck },
-                ID = Guid.NewGuid().ToString(),
-                Name = serviceName,
-                Address = serviceAddress,
-                Tags = new[] { $"urlprefix-/{serviceName}" }//Add a tag tag in the format of urlprefix-/servicename so that Fabio can recognize it
-            };
-
-            consulClient.Agent.ServiceRegister(registration).Wait();//Register when the service starts, the internal implementation is actually to register using the Consul API (initiated by HttpClient)
-            lifetime.ApplicationStopping.Register(() =>
-            {
-                consulClient.Agent.ServiceDeregister(registration.ID).Wait();//Unregister when the service stops
+                endpoints.MapHealthChecks("/HealthCheck");
             });
         }
     }
